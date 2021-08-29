dc82f6c66dfddcefdd60bcdd35b9b91f614c1d9b
black-code1 <legrandmahamat1@gmail.com>
2021-08-29 21:08:26 +0100

    Auth Routes & Controllers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -12,14 +13,15 @@ app.set("view engine", "ejs");
 // database connection
 const dbURI = "mongodb://localhost:27017/node-auth";
 mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+	.connect(dbURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then((result) => app.listen(3000))
+	.catch((err) => console.log(err));
 
 // routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
