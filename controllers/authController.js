b07bfed35412bbbf6619032ab1eb05db6d8a0346
black-code1 <legrandmahamat1@gmail.com>
2021-09-01 04:00:19 +0100

    Mongoose Validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,25 @@
 const User = require('../models/User')
 
+// handle errors
+const handleErrors = (error) => {
+	console.log(error.message, error.code)
+	let errors = { email: '', password: '' }
+
+	// duplicate error code
+	if (error.code === 11000) {
+		errors.email = 'that email is already registered'
+		return errors
+	}
+
+	// validation errors
+	if (error.message.includes('user validation failed')) {
+		Object.values(error.errors).forEach(({ properties }) => {
+			errors[properties.path] = properties.message
+		})
+	}
+
+	return errors
+}
 module.exports.signup_get = (req, res) => {
 	res.render("signup");
 };
@@ -16,8 +36,8 @@ module.exports.signup_post = async (req, res) => {
 		const user = await User.create({ email, password })
 		res.status(201).json(user)
 	} catch (error) {
-		console.log(error)
-		res.status(400).send('error, user not created')
+		const errors = handleErrors(error)
+		res.status(400).json({ errors })
 	}
 };
 
