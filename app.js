b0830167bbab65f8e049bc9c07aa0a9a4ddcf27f
black-code1 <legrandmahamat1@gmail.com>
2021-09-04 14:12:47 +0100

    Cookies Primer

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser())
 
 // view engine
 app.set("view engine", "ejs");
@@ -25,4 +27,24 @@ mongoose
 // routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+	//res.setHeader('Set-Cookie', 'newUser=true');
+
+	//name->value
+	res.cookie('newUser', false)
+
+	//cookie send if secure that is https secure: true or httpOnly:true
+	res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+
+	res.send('you got the cookie!')
+})
+
+app.get('/read-cookies', (req, res) => {
+	const cookies = req.cookies
+	console.log(cookies)
+
+	res.json(cookies.newUser)
+})
 app.use(authRoutes);
