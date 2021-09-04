f608179409436ad8c2b2bd2453beb8e07f6c1482
black-code1 <legrandmahamat1@gmail.com>
2021-09-04 15:04:39 +0100

    New User Signup (part1)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,34 @@
-const User = require('../models/User')
-
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 // handle errors
 const handleErrors = (error) => {
-	console.log(error.message, error.code)
-	let errors = { email: '', password: '' }
+	console.log(error.message, error.code);
+	let errors = { email: "", password: "" };
 
 	// duplicate error code
 	if (error.code === 11000) {
-		errors.email = 'that email is already registered'
-		return errors
+		errors.email = "that email is already registered";
+		return errors;
 	}
 
 	// validation errors
-	if (error.message.includes('user validation failed')) {
+	if (error.message.includes("user validation failed")) {
 		Object.values(error.errors).forEach(({ properties }) => {
-			errors[properties.path] = properties.message
-		})
+			errors[properties.path] = properties.message;
+		});
 	}
 
-	return errors
-}
+	return errors;
+};
+
+//days * hours * minutes * seconds
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+	return jwt.sign({ id }, "coder secret", {
+		expiresIn: maxAge,
+	});
+};
+
 module.exports.signup_get = (req, res) => {
 	res.render("signup");
 };
@@ -31,13 +40,15 @@ module.exports.login_get = (req, res) => {
 module.exports.signup_post = async (req, res) => {
 	const { email, password } = req.body;
 
-
 	try {
-		const user = await User.create({ email, password })
-		res.status(201).json(user)
+		const user = await User.create({ email, password });
+		const token = createToken(user._id);
+		// 3 days
+		res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+		res.status(201).json({ user: user._id });
 	} catch (error) {
-		const errors = handleErrors(error)
-		res.status(400).json({ errors })
+		const errors = handleErrors(error);
+		res.status(400).json({ errors });
 	}
 };
 
